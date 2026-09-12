653e0e7803d809bebfcd134d177e61b5eddc0c80
Ravi Singh <ravi.ranjay.singh.611@gmail.com>
2026-09-12 18:58:50 +0530

    Fix for production hosting

---
+++ run (1).py
@@ -0,0 +1,9 @@
+import os
+from app import create_app
+from app.extensions import socketio
+
+app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, debug=False, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
